7d534384aee997b269ce188f267e89d2b45d0fee
Hải Nguyễn <hainguyen.ts13@gmail.com>
2025-05-31 23:31:33 +0700

    feat: Implement SoundpackSelector component for keyboard and mouse soundpacks

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -6,6 +6,7 @@ pub mod logo;
 pub mod mouse_soundpack_selector;
 pub mod mouse_volume_slider;
 pub mod pages;
+pub mod soundpack_selector;
 pub mod theme_toggler;
 pub mod titlebar;
 pub mod volume_slider;
