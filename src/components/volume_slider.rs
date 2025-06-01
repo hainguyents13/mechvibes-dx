9bc3ac935841759a274cc92fb89b9ce2450adcec
Hải Nguyễn <hainguyen.ts13@gmail.com>
2025-06-01 16:38:07 +0700

    feat: Add tooltip to VolumeSlider component and adjust layout classes

--- src/components/volume_slider.rs
+++ src/components/volume_slider.rs
@@ -1,9 +1,10 @@
 use dioxus::prelude::*;
+use lucide_dioxus::Volume2;
 
 #[component]
 pub fn VolumeSlider(volume: Signal<f32>, on_change: Option<EventHandler<f32>>) -> Element {
     rsx! {
-      div { class: "grid grid-cols-12 gap-4",
+      div { class: "grid grid-cols-12",
         div { class: "rounded col-span-4 flex items-center",
           label {
             r#for: "volume-slider",
@@ -12,9 +13,9 @@ pub fn VolumeSlider(volume: Signal<f32>, on_change: Option<EventHandler<f32>>) -
           }
           span { class: "font-bold ml-1", "{(volume() * 100.0) as u8}%" }
         }
-        div { class: "col-span-8",
+        div { class: "col-span-8 flex items-center gap-2",
           input {
-            class: "range range-xs",
+            class: "range range-xs grow",
             r#type: "range",
             min: 0.0,
             max: 1.0,
@@ -29,6 +30,13 @@ pub fn VolumeSlider(volume: Signal<f32>, on_change: Option<EventHandler<f32>>) -
                     }
                 }
             },
+          }
+          div { class: "tooltip", "data-tip": "Mute",
+            button {
+              class: "btn btn-square btn-ghost",
+              onclick: move |_| {},
+              Volume2 { class: "w-5 h-5" }
+            }
           }
         }
       }
